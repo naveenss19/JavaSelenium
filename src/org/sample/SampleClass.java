abe7052d3d1fe17bb4a0cac5723e14cfe63a0f6e
naveenss19 <naveenvip100@gmail.com>
2025-07-06 15:36:43 +0530

    code added

---
+++ src/org/sample/SampleClass.java
@@ -0,0 +1,20 @@
+package org.sample;
+
+public class SampleClass {
+
+	private void sampleMethod() {
+
+		System.out.println("Naveen is creating a Method with Sysout");
+
+	}
+
+	public static void main(String[] args) {
+
+	SampleClass a = new SampleClass();
+	
+	a.sampleMethod();
+	
+	
+	}
+
+}
